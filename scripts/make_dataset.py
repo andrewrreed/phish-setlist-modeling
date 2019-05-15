27d0815793ac12237f548e30ce41f9b1c608eea8
Andrew Reed <areed1242@gmail.com>
2019-05-15 16:56:56 -0400

    Update make_dataset.py

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -41,6 +41,10 @@ X_data, y_data = create_sequence_modeling_data(encoded_setlist_list)
 # create test/train split (Note - unable to stratify because some songs only occur once)
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.1, random_state=2)
 
+# pickle the song encoding mappings
+create_pickle_object(obj= song_to_idx, pickle_name='song_to_idx.pickle', file_path='../data/processed/mvp-setlist-modeling/')
+create_pickle_object(obj= idx_to_song, pickle_name='idx_to_song.pickle', file_path='../data/processed/mvp-setlist-modeling/')
+
 # pickle training data
 create_pickle_object(obj= X_train, pickle_name='X_train.pickle', file_path='../data/processed/mvp-setlist-modeling/')
 create_pickle_object(obj= X_test, pickle_name='X_test.pickle', file_path='../data/processed/mvp-setlist-modeling/')
@@ -49,14 +53,14 @@ create_pickle_object(obj= y_test, pickle_name='y_test.pickle', file_path='../dat
 
 # one-hot encode training data
 num_classes = len(song_to_idx)
-
 X_train_hot = np.array([to_categorical(x, num_classes=num_classes) for x in X_train])
 X_test_hot = np.array([to_categorical(x, num_classes=num_classes) for x in X_test])
 y_train_hot = to_categorical(y_train, num_classes=num_classes)
 y_test_hot = to_categorical(y_test, num_classes=num_classes)
 
 # pickle one-hot encoded training data
-create_pickle_object(obj= X_train_hot, pickle_name='X_train_hot.pickle', file_path='../data/processed/mvp-setlist-modeling/')
-create_pickle_object(obj= X_test_hot, pickle_name='X_test_hot.pickle', file_path='../data/processed/mvp-setlist-modeling/')
+## NOTE - one-hot encoded X datasets are too large to pickle AND are not needed for modeling
+#create_pickle_object(obj= X_train_hot, pickle_name='X_train_hot.pickle', file_path='../data/processed/mvp-setlist-modeling/')
+#create_pickle_object(obj= X_test_hot, pickle_name='X_test_hot.pickle', file_path='../data/processed/mvp-setlist-modeling/')
 create_pickle_object(obj= y_train_hot, pickle_name='y_train_hot.pickle', file_path='../data/processed/mvp-setlist-modeling/')
 create_pickle_object(obj= y_test_hot, pickle_name='y_test_hot.pickle', file_path='../data/processed/mvp-setlist-modeling/')
