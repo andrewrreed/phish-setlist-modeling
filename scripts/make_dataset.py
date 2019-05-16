7528975eec324dad5f0b8263614bfe714e794107
Andrew Reed <areed1242@gmail.com>
2019-05-16 09:43:36 -0400

    create argument for seq length

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -4,9 +4,11 @@ Build MVP Modeling Data
 
 The following script can be used to recreate the exact data used in MVP modeling.
 To recreate the data, run the the following command from the terminal with the 
-current working directory as 'phish-setlist-modeling/scriptsw':
+current working directory as 'phish-setlist-modeling/scripts' and pass a desired sequence length:
 
-python make_dataset.py
+python make_dataset.py <sequence-length>
+
+Ex: python make_dataset.py 100
 
 '''
 import os
@@ -23,6 +25,8 @@ if module_path not in sys.path:
 from src.util import load_pickle_object, create_pickle_object
 from src.process import preprocess_data, create_song_encodings, encode_setlist_data, create_sequence_modeling_data
 
+# store CLI arg
+sequence_length = int(sys.argv[1])
 
 # load raw setlist data
 all_setlists = load_pickle_object(r'../data/raw/extract-05032019/all_setlists.pkl')
@@ -37,20 +41,20 @@ song_to_idx, idx_to_song = create_song_encodings(setlist_list)
 encoded_setlist_list = encode_setlist_data(song_to_idx, setlist_list)
 
 # create X and y sequence data
-X_data, y_data = create_sequence_modeling_data(encoded_setlist_list)
+X_data, y_data = create_sequence_modeling_data(encoded_setlist_list, seq_length=sequence_length)
 
 # create test/train split (Note - unable to stratify because some songs only occur once)
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.1, random_state=2)
 
 # pickle the song encoding mappings
-create_pickle_object(obj= song_to_idx, pickle_name='song_to_idx.pkl', file_path='../data/processed/mvp-setlist-modeling/')
-create_pickle_object(obj= idx_to_song, pickle_name='idx_to_song.pkl', file_path='../data/processed/mvp-setlist-modeling/')
+create_pickle_object(obj= song_to_idx, pickle_name='song_to_idx.pkl', file_path=f'../data/processed/mvp-setlist-modeling/seqlen-{sequence_length}/')
+create_pickle_object(obj= idx_to_song, pickle_name='idx_to_song.pkl', file_path=f'../data/processed/mvp-setlist-modeling/seqlen-{sequence_length}/')
 
 # pickle training data
-create_pickle_object(obj= X_train, pickle_name='X_train.pkl', file_path='../data/processed/mvp-setlist-modeling/')
-create_pickle_object(obj= X_test, pickle_name='X_test.pkl', file_path='../data/processed/mvp-setlist-modeling/')
-create_pickle_object(obj= y_train, pickle_name='y_train.pkl', file_path='../data/processed/mvp-setlist-modeling/')
-create_pickle_object(obj= y_test, pickle_name='y_test.pkl', file_path='../data/processed/mvp-setlist-modeling/')
+create_pickle_object(obj= X_train, pickle_name='X_train.pkl', file_path=f'../data/processed/mvp-setlist-modeling/seqlen-{sequence_length}/')
+create_pickle_object(obj= X_test, pickle_name='X_test.pkl', file_path=f'../data/processed/mvp-setlist-modeling/seqlen-{sequence_length}/')
+create_pickle_object(obj= y_train, pickle_name='y_train.pkl', file_path=f'../data/processed/mvp-setlist-modeling/seqlen-{sequence_length}/')
+create_pickle_object(obj= y_test, pickle_name='y_test.pkl', file_path=f'../data/processed/mvp-setlist-modeling/seqlen-{sequence_length}/')
 
 # one-hot encode training data
 num_classes = len(song_to_idx)
@@ -63,5 +67,5 @@ y_test_hot = to_categorical(y_test, num_classes=num_classes)
 ## NOTE - one-hot encoded X datasets are too large to pickle AND are not needed for modeling
 #create_pickle_object(obj= X_train_hot, pickle_name='X_train_hot.pkl', file_path='../data/processed/mvp-setlist-modeling/')
 #create_pickle_object(obj= X_test_hot, pickle_name='X_test_hot.pkl', file_path='../data/processed/mvp-setlist-modeling/')
-create_pickle_object(obj= y_train_hot, pickle_name='y_train_hot.pkl', file_path='../data/processed/mvp-setlist-modeling/')
-create_pickle_object(obj= y_test_hot, pickle_name='y_test_hot.pkl', file_path='../data/processed/mvp-setlist-modeling/')
+create_pickle_object(obj= y_train_hot, pickle_name='y_train_hot.pkl', file_path=f'../data/processed/mvp-setlist-modeling/seqlen-{sequence_length}/')
+create_pickle_object(obj= y_test_hot, pickle_name='y_test_hot.pkl', file_path=f'../data/processed/mvp-setlist-modeling/seqlen-{sequence_length}/')
